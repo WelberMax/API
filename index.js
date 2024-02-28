d5584fa8a6405ceba0db3c6bf8da8a7f03b6e0d6
WelberMax <welbermax@gmail.com>
2024-02-28 16:20:38 +0100

    new update

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-require("dotenv").config("../.env.");
+require("dotenv").config("./.env.");
 const PORT = process.env.REACT_APP_PORT;
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 
 app.use(express.json());
 app.use(cors());
 
 //API ROUTES
 //Projects
-const Routes = require("../routes/router");
+const Routes = require("./routes/router");
 app.use("/api", Routes);
 
 //DB CONNECTION
-const conn = require("./conn")
-conn()
+const connectDatabase = require("./db/conn")
+connectDatabase()
 
 
 
