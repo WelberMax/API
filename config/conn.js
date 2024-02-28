90729d19449850b77ca10e93c3d4a8c530929ed5
WelberMax <welbermax@gmail.com>
2024-02-28 12:38:29 +0100

    Funcional

---
+++ config/conn.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require("dotenv").config("../.env.");
+
+const dbUser = process.env.REACT_APP_DB_USER;
+const dbPass = process.env.REACT_APP_DB_PASS;
+
+
+async function main() {
+    try {
+        mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@welberdatatabase.776qxzb.mongodb.net/test?retryWrites=true&w=majority&appName=welberdatatabase`)
+        console.log('MongoDB Conectado a projetos!')        
+    } catch (error) {
+        console.error(`error`, error);
+    }
+}
+
+
+module.exports = main;
